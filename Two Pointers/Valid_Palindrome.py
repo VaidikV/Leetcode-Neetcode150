5461b3cb6ac859bc2eef827372432d8e5caf53a9
Vaidik V <vaid.ik.vvv@gmail.com>
2024-08-21 16:29:51 -0700

    Structured files

--- Two Pointers/Valid_Palindrome.py
+++ Two Pointers/Valid_Palindrome.py
@@ -15,10 +15,8 @@ def isPalindrome(s: str) -> bool:
     while l < r:
         while l < r and not alphaNum(s[l]):
             l += 1
-
-        while r < l and not alphaNum(s[l]):
+        while r > l and not alphaNum(s[r]):
             r -= 1
-
         if s[l].lower() != s[r].lower():
             return False
         l, r = l + 1, r - 1
@@ -30,5 +28,5 @@ def alphaNum(c):
             ord('a') <= ord(c) <= ord('z') or
             ord('0') <= ord(c) <= ord('9'))
 
-print(isPalindrome("sugus"))
+print(isPalindrome("A man, a plan, a canal: Panama"))
 
