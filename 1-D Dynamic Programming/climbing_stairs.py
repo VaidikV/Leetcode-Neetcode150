c0ea94d41e6612d7abda2eda9bb4161afb9028a3
Vaidik V <vaid.ik.vvv@gmail.com>
2024-10-26 14:07:47 -0700

    Added more problems

---
+++ 1-D Dynamic Programming/climbing_stairs.py
@@ -0,0 +1,21 @@
+
+def climbStairs(n):
+    # one, two = 1, 1
+    #
+    # for i in range(n-1):
+    #     temp = one
+    #     one = one + two
+    #     two = temp
+    #
+    # return one
+
+# practice
+    one, two = 1, 1
+    for i in range(n-1):
+        temp = one
+        one = one + two
+        two = temp
+    return one
+
+n = 5
+print(climbStairs(n))
