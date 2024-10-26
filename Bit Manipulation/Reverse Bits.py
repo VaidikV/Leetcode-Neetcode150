5e4bdc8e130d9c7355cb439690189314eb95d00b
Vaidik V <vaid.ik.vvv@gmail.com>
2024-10-26 13:18:33 -0700

    Added more problems

--- Bit Manipulation/Reverse Bits.py
+++ Bit Manipulation/Reverse Bits.py
@@ -1,9 +1,18 @@
 def reverseBits(n):
-    res = 0
+    # res = 0
+    #
+    # for i in range(32):
+    #     res = res << 1 # right shift eg. 0 -> 00
+    #     bit = n % 2 # extract least (rightmost bit) eg 101 -> 1
+    #     res += bit
+    #     n = n >> 1
+    #
+    # return res
 
+    res = 0
     for i in range(32):
-        res = res << 1 # right shift eg. 0 -> 00
-        bit = n % 2 # extract least (rightmost bit) eg 101 -> 1
+        res = res << 1
+        bit = n % 2
         res += bit
         n = n >> 1
 
